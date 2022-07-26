8bffbcae2695c74cdb516d4c28e0291698763c6a
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-07-26 14:48:55 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
 import React, { Component } from 'react';
+import './nprogress.css';
 import './App.css';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
+import { extractLocations, getEvents } from './api';
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+    numberOfEvents: 32,
+    savedLocation: 'all',
+  };
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  updateEvents = (location, eventCount) => {
+    getEvents().then((events) => {
+      let locationEvents;
+      if (location === undefined) {
+        locationEvents = events.slice(0, eventCount);
+      } else if (eventCount === undefined) {
+        location =
+          location === 'all' ? events : events.filter((event) => event.location === location);
+      }
+
+      this.setState({
+        events: locationEvents,
+        numberOfEvents: eventCount,
+        savedLocation: location,
+      });
+    });
+  };
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <EventList events={this.state.events} />
+        <NumberOfEvents
+          numberOfEvents={this.state.numberOfEvents}
+          updateEvents={this.updateEvents}
+        />
+      </div>
+    );
   }
 }
 

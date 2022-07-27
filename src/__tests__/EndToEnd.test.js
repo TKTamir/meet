04c31af92a87724426b43375882523b6ec9cc78a
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-07-27 17:02:33 +0300

    Add EndToEnd test

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer';
+import { mockData } from '../mock-data';
 
 describe('show/hide an event details', () => {
   jest.setTimeout(30000);
@@ -36,4 +37,34 @@ describe('show/hide an event details', () => {
     const eventDetails = await page.$('.event .event-details');
     expect(eventDetails).toBeNull();
   });
+});
+
+describe('Filter events by city', () => {
+  jest.setTimeout(30000);
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch(/*{
+        headless: false,
+        slowMo: 150,
+        ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
+      }*/);
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+  test('When a user has not searched for a city, show upcoming events from all cities.', async () => {
+    const eventCount = (await page.$$('.event')).length;
+    expect(eventCount).toBe(mockData.length);
+  });
+  //   test('User should see a list of suggestions when they search for a city.', async () => {
+  //     await page.type('.city', 'Berlin', { delay: 150 });
+  //     const suggestions = await page.$$('.suggestions li');
+  //     expect(suggestions).toHaveLength(2);
+  //   });
 });
